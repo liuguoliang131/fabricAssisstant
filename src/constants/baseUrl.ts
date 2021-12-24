25c865489bca319a75d9ab1f11c98d906d7a3e60
xupanqiang <541592004@qq.com>
2021-12-24 13:43:54 +0800

    优化

--- src/constants/baseUrl.ts
+++ src/constants/baseUrl.ts
@@ -1,5 +1,9 @@
-//后端api url
-export const API_BASE_URL = 'http://192.168.0.250:8096/';
+//后端api url 生产
+// export const API_BASE_URL = 'http://factory.forniao.cn';
+//后端api url 测试
+export const API_BASE_URL = 'http://factory.ouryou.cn';
+//后端api url 开发
+// export const API_BASE_URL = 'http://192.168.0.250:8096';
 //前端访问地址
 export const BASE_URL = 'http://localhost:3001';
 //打包文件夹
