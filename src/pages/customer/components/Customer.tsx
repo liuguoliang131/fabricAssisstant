d13e47daea8c74fe808de27ca119117f8e926cfd
xupanqiang <541592004@qq.com>
2021-12-31 12:39:26 +0800

    优化

--- src/pages/customer/components/Customer.tsx
+++ src/pages/customer/components/Customer.tsx
@@ -66,6 +66,7 @@ const Customer: FC = (): ReactElement => {
         grid: {
             left: 0,
             bottom: 0,
+            right: 0,
             containLabel: true
         },
         xAxis: {
