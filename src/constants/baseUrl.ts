a6b3de5061603716b1ec4bce0994fc0a239ae4c9
xupanqiang <541592004@qq.com>
2022-01-07 16:26:06 +0800

    优化

--- src/constants/baseUrl.ts
+++ src/constants/baseUrl.ts
@@ -1,7 +1,7 @@
 //后端api url 生产
-// export const API_BASE_URL = 'http://factory.forniao.cn';
+export const API_BASE_URL = 'http://factory.forniao.cn';
 //后端api url 测试
-export const API_BASE_URL = 'http://factory.ouryou.cn';
+// export const API_BASE_URL = 'http://factory.ouryou.cn';
 //后端api url 开发
 // export const API_BASE_URL = 'http://192.168.0.250:8096';
 //前端访问地址
